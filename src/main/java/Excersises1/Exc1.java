ddcec97156fc5b7be2c8904ab59010c9b014b8c3
pawelpawel8787 <bassista@interia.pl>
2019-04-02 22:15:38 +0200

    first commit

---
+++ src/main/java/Excersises1/Exc1.java
@@ -0,0 +1,17 @@
+package Excersises1;
+
+public class Exc1 {
+
+    public static void main(String[] args) {
+
+        int num1, num2;
+
+        num1 = 10;
+
+        num2 = num1;
+
+        System.out.println(num1==num2);
+
+    }
+
+}
