c4ad1e9f1e1311dae3bf52be086b7605e15a4efe
pawelpawel8787 <bassista@interia.pl>
2019-04-03 22:22:24 +0200

    create random second class for tests

---
+++ src/main/java/Excersises1/Exc2.java
@@ -0,0 +1,23 @@
+package Excersises1;
+
+import java.util.Random;
+
+public class Exc2 {
+
+    public static void main(String[] args) {
+
+        int [] array = new int[10];
+
+        Random random = new Random(20);
+        for (int i = 0; i <10 ; i++) {
+
+            array[i] = random.nextInt(50);
+        }
+
+        for (int resultArray: array
+             ) {
+            System.out.println(resultArray + " ");
+        }
+    }
+
+}
