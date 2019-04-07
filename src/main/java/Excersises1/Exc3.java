b403e886aa54511341968b5fd953dd8349428abb
pawelpawel8787 <gitpawelo@wp.pl>
2019-04-07 19:18:01 +0200

    Exc3 class comlpeted.

--- src/main/java/Excersises1/Exc3.java
+++ src/main/java/Excersises1/Exc3.java
@@ -1,4 +1,30 @@
 package Excersises1;
 
+/*
+2.a Napisz program, w którym zadeklarujesz zmienne wszystkich typów prostych, ale nie przypisuj im wartości jako stałej,
+ale wynik jakiegoś wyrażenia. Wypisz te zmienne w konsoli.
+ */
 public class Exc3 {
+
+    public static void main(String[] args) {
+
+        byte varByte;
+        short varShort;
+        int varInt;
+        double varDouble;
+        float varFloat;
+
+        varByte = 100 + 27;
+        varShort = (short) (30000 + varByte);
+        varInt = varByte + varShort;
+        varFloat = varInt + varShort;
+        varDouble = varFloat + varInt;
+
+        System.out.println("Byte: " + varByte + "\n" +
+                "Short: " + varShort + "\n" +
+                "Integer: " + varInt + "\n" +
+                "Double: " + varDouble + "\n" +
+                "Float: " + varFloat);
+    }
+
 }
