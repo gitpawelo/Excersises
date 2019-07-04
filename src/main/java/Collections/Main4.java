4be1046c7aac1f7189ec02b824d3969ffa0d63d5
pawelpawel8787 <gitpawelo@wp.pl>
2019-07-04 21:44:49 +0200

    Add 2 classes with collection excersises

--- src/main/java/Collections/Main4.java
+++ src/main/java/Collections/Main4.java
@@ -5,5 +5,33 @@ może być wykorzystywana wielokrotnie. Korzystająć z odpowiedniej kolekcji do
 w kolekcji się nie powtarzały. Podczas dodawania dodaj kilka imion powtarzających się.
  */
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main4 {
+
+    public static void main(String[] args) {
+
+        Set<String> nameMatrix = new HashSet<>();
+
+        nameMatrix.add("Adam");
+        nameMatrix.add("Karolina");
+        nameMatrix.add("Asia");
+        nameMatrix.add("Anatol");
+        nameMatrix.add("Andrzej");
+        nameMatrix.add("Halina");
+        nameMatrix.add("Waldek");
+        nameMatrix.add("Krzysztof");
+        nameMatrix.add("Eufenia");
+        nameMatrix.add("Adam");
+        nameMatrix.add("Zenon");
+        nameMatrix.add("Adam");
+
+        System.out.println(nameMatrix.size());
+
+        for (String result: nameMatrix
+             ) {
+            System.out.print(result + ", ");
+        }
+    }
 }
--- src/main/java/Collections/Main5.java
+++ src/main/java/Collections/Main5.java
@@ -5,5 +5,27 @@ Wykorzstaj do tego odpowiednią kolekcję. Dodaj 10 zadań, a następnie pobierz
 w jakiej zostały do niej dodane.
  */
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Main5 {
+
+    public static void main(String[] args) {
+
+        Queue<String> sequences = new ArrayDeque<>();
+
+        sequences.add("Zrób A!");
+        sequences.add("Zrób B!");
+        sequences.add("Zrób C!");
+        sequences.add("Zrób D!");
+        sequences.add("Zrób E!");
+        sequences.add("Zrób F!");
+        sequences.add("Zrób G!");
+
+        for (String result: sequences
+             ) {
+            System.out.println(result+ ", ");
+        }
+    }
+
 }
